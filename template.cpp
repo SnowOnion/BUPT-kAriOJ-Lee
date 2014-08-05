54851d62741fe3af61202fb568df23d87b6d32b6
tianchi.ltc <tianchi.ltc@alibaba-inc.com>
2014-08-05 15:45:00 +0800

    修改template, 宏实现表打印

---
+++ template.cpp
@@ -0,0 +1,84 @@
+#include<memory.h>
+
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <stack>
+#include <queue>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+const double eps=1e-8;
+const double pi=atan(1.0)*4;
+const int inf=2147483600;
+int pow10[7]= {1,10,100,1000,10000,100000,1000000};
+
+#define f0n_1(_x,n) for(int _x=0;_x<n;_x++)
+#define f1n_1(_x,n) for(int _x=1;_x<n;_x++)
+#define f0n(_x,n) for(int _x=0;_x<=n;_x++)
+#define f1n(_x,n) for(int _x=1;_x<=n;_x++)
+
+#define printArray(lst,len) f0n_1(_i,(len)){printf("%d ",lst[_i]);} puts("");
+#define printMatrix(mat,row,col) f0n_1(_j,(row)){printArray(mat[_j],(col));}
+
+int inint(int* in){
+    return scanf("%d",in);
+}
+
+int indouble(double* in){
+    return scanf("%lf",in);
+}
+
+//void printarray(int* lst,int len){
+//    for(int i=0;i<len;i++){
+//        printf("%d ",lst[i]);
+//    }
+//    puts("");
+//}
+//
+///** Totally can't work. Use Marco instead.
+//    ... wow function sucks macro rocks
+// */
+//void printmatrix(int** mat, int row, int col){
+//    for(int i=0;i<row;i++){
+//        //printarray(*(mat+i*col),col);
+//        printarray(*(mat+i),col); // true pointer plus... // never successfully runned so never tested.
+//    }
+//}
+
+void test(){
+    int a[2][3]={{1,2,3},{4,5,6}};
+    int b[][3]={9,8,7,6,5,4};
+
+    printArray(a[1],3);
+    printMatrix(a,2,3);
+    printMatrix(b,2,3);
+}
+
+void init(){
+
+}
+
+void solve(){
+    int cases;
+    inint(&cases);
+    while(cases--){
+
+    }
+}
+
+int main(){
+    test();
+    init();
+    solve();
+
+    return 0;
+}
