9de8551e01251eccec7fe8a8bba836f180597205
SnowOnion <SnowOnionlee@gmail.com>
2014-08-07 10:29:28 +0800

    骗分法大战卡常数

--- kari449-排序.cpp
+++ kari449-排序.cpp
@@ -14,29 +14,69 @@ std::sort TLE
 数据取值[0,10000], 考虑桶排序
 写了个朴素的桶排序, T. 不知道怎么办...
 数组改成map, 还是T.
+
+std::map 的实现是红黑树, 访问 O(log n). http://bbs.byr.cn/#!article/ACM_ICPC/80885
+"其实两组1000000卡sort，1000000组5卡计数～
+有一种很丑的姿势过这题就是
+if n>200000: 用计数
+else: 用sort "
+↑ It works!!!!!!! 骗分第一课...
 */
 
-int main(){
+int main()
+{
     int n;
-    //int radix[10010];
-    map<int,int> radix;
+    int radix[10010];
+    vector<int> v;
+    int vv;
     int in;
 
-    while(scanf("%d",&n)>0){
-        //memset(radix,0,sizeof(radix));
-        radix.clear();
-        for(int i=0;i<n;i++){
-            scanf("%d",&in);
-            radix[in]+=1;
-        }
+    while(scanf("%d",&n)>0)
+    {
+        int casefirst=1;
+        if(n>200000)
+        {
+            memset(radix,0,sizeof(radix));
 
-        //for(int i=0;i<=10000;i++){
-        for(map<int,int>::iterator iter=radix.begin();iter!=radix.end();++iter){
-            for(int j=0;j<iter->second;j++){
-                printf("%d ",iter->first);
+            for(int i=0; i<n; i++)
+            {
+                scanf("%d",&in);
+                radix[in]+=1;
             }
+
+            for(int i=0; i<=10000; i++)
+            {
+                for(int j=0; j<radix[i]; j++)
+                {
+                    if(casefirst){
+                        casefirst=0;
+                        printf("%d",i);
+                    }
+                    else{
+                        printf(" %d",i);
+                    }
+
+                }
+            }
+            printf("\n");
         }
-        printf("\n");
+        else
+        {
+            v.clear();
+            for(int i=0; i<n; i++)
+            {
+                scanf("%d",&vv);
+                v.push_back(vv);
+            }
+
+            sort(v.begin(),v.begin()+n);
+
+            for(int i=0; i<n-1; i++)
+            {
+                printf("%d ",v[i]);
+            }
+            printf("%d\n",v[n-1]);
+        }
     }
     return 0;
 }
